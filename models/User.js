efafbf434c23877faca9e224a6d73095f6ad208d
HassanH-ussain <104021801+HassanH-ussain@users.noreply.github.com>
2024-11-25 23:50:35 +0000

    Fix

--- models/User.js
+++ models/User.js
@@ -5,19 +5,11 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   email: { type: String, required: true }
   // Add resume fields here
-<<<<<<< HEAD
   // phone: { type: String },
   // profession: { type: String },
   // jobDesc: { type: String },
   // school: { type: String },
   // gpa: { type: String }
-=======
-  phone: { type: String },
-  profession: { type: String },
-  jobDesc: { type: String },
-  school: { type: String },
-  gpa: { type: String }
->>>>>>> parent of 8bb7478 (good job)
 });
 
 const User = mongoose.model('User', userSchema);
