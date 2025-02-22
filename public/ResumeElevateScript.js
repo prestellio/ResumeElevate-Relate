bc8e23ecbeb9ab1ca3b42b65888867064a5db53e
Preston Elliott <149803791+prestellio@users.noreply.github.com>
2025-02-22 06:04:09 +0000

    Index overhaul and stylesheet overhaul, personalized script, and new images.

---
+++ public/ResumeElevateScript.js
@@ -0,0 +1,7 @@
+
+//Moves the user to the generate.html page
+function toGenerator() {
+    window.location.href="generate.html";
+}
+//Button with id="generator" in index.html
+document.getElementById("generator").addEventListener("click", toGenerator);
