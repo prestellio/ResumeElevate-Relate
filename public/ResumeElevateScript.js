8e60898b468eb36e6b767028fe599b2d9b3282d7
Preston Elliott <149803791+prestellio@users.noreply.github.com>
2025-02-25 01:06:23 +0000

    Index.html mostly cleaned, new about.html being developed

--- public/ResumeElevateScript.js
+++ public/ResumeElevateScript.js
@@ -5,3 +5,22 @@ function toGenerator() {
 }
 //Button with id="generator" in index.html
 document.getElementById("generator").addEventListener("click", toGenerator);
+
+//Nav bar collapse
+function toggleNav() {
+    var navList = document.getElementById("nav-btn");
+    if (navList.style.display === "none" || navList.style.display === "") {
+        navList.style.display = "flex";
+    } else {
+        navList.style.display = "none";
+    }
+}
+
+document.getElementById('navbarToggle').addEventListener('click', function() {
+    var menu = document.getElementById('navbarMenu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
