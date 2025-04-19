f3dacd5c93c3f1ca6faff2412db4a74d37a7eb99
Preston Elliott <149803791+prestellio@users.noreply.github.com>
2025-04-19 23:14:19 +0000

    Added template and updated file locations and paths

--- app.js
+++ app.js
@@ -238,4 +238,4 @@ app.use((req, res) => {
     res.status(404).json({ error: 'Route not found' });
 });
 
-app.listen(port, () => console.log(`Server running at http://localhost:${port}/`));
+app.listen(port, () => console.log(`Server running at http://localhost:${port}/pages/`));
